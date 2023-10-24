a695af8dd51551750a7f0880bff828c69364a994
DawidStepniewskii <dawid.stepniewski.ds@gmail.com>
2023-10-24 18:12:45 +0200

    Add: Added login authorization + logout feature

--- ppp_django_project/news/urls.py
+++ ppp_django_project/news/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import index, add, get
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', index, name='view_news'),
     path('add/', add, name="add"),
     path('<int:id>/', get, name='get'),
 ]
